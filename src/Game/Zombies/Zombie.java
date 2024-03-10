e5dc174c6430feb2607150acf99e42d9daa98114
V <lytranvinh.work@gmail.com>
2024-03-10 23:30:38 +0700

    [Upgrade]: câú trúc lại code

--- src/Game/Zombies/Zombie.java
+++ src/Game/Zombies/Zombie.java
@@ -5,13 +5,11 @@ import javafx.util.Pair;
 import src.Game.GameElements;
 
 public class Zombie extends GameElements {
-    private int speed;
-    private int hp;
-    private int dame;
-    public Timeline moveZombie;
+    private int speed; // Tốc độ di chuyển của zombie
+    private int hp;  // Máu của zombie
+    private int dame; // Sát thương của zombie
+    public Timeline moveZombie; // Hành động di chuyển của zombie
     private boolean flag = true; // true : zombie đang di chuyển, false: zombie dừng lại tấn công.
-
-
     public Zombie() {
         super();
     }
@@ -21,28 +19,13 @@ public class Zombie extends GameElements {
         this.hp = hp;
         this.dame = dame;
     }
-    public int getSpeed() {
-        return speed;
-    }
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
-    public int getHp() {
-        return hp;
-    }
-    public void setHp(int hp) {
-        this.hp = hp;
-    }
-    public int getDame() {
-        return dame;
-    }
-    public void setDame(int dame) {
-        this.dame = dame;
-    }
+    // Di chuyển zombie
     public void move() {
     }
+    // Tấn công plant
     public void attack() {
     }
+    // Chuyển từ lane sang layoutY
     public static int laneToLayoutY(int l) {
         int LayoutY = 0;
         switch (l) {
@@ -66,21 +49,35 @@ public class Zombie extends GameElements {
         }
         return LayoutY;
     }
-
+    // Get và set các thuộc tính
     public Timeline getMoveZombie() {
         return moveZombie;
     }
-
     public void setMoveZombie(Timeline moveZombie) {
         this.moveZombie = moveZombie;
     }
-
     public boolean getFlag() {
         return flag;
     }
-
     public void setFlag(boolean flag) {
         this.flag = flag;
     }
-
+    public int getSpeed() {
+        return speed;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    public int getHp() {
+        return hp;
+    }
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+    public int getDame() {
+        return dame;
+    }
+    public void setDame(int dame) {
+        this.dame = dame;
+    }
 }
