c66893be37dfacd5afa3ee7e301b673c9b5bab50
V <lytranvinh.work@gmail.com>
2024-05-16 21:59:19 +0700

    [Upgrade]: Img ConeheadZombie

--- src/Config/Path.java
+++ src/Config/Path.java
@@ -34,8 +34,8 @@ public class Path {
     // Assets - Images Zombies
     public static final String ASSETS_Image_NormalZombie_Run = "/Assets/images/Zombies/NormalZombieRun.gif";
     public static final String ASSETS_Image_NormalZombie_Eat = "/Assets/images/Zombies/NormalZombieEat.gif";
-    public static final String ASSETS_Image_ConeheadZombie_Run = "/Assets/images/Zombies/NormalZombieRun.gif"; // todo chua co dung tam
-    public static final String ASSETS_Image_ConeheadZombie_Eat = "/Assets/images/Zombies/NormalZombieRun.gif"; // todo chua co dung tam
+    public static final String ASSETS_Image_ConeheadZombie_Run = "/Assets/images/Zombies/ConeheadZombie.gif";
+    public static final String ASSETS_Image_ConeheadZombie_Eat = "/Assets/images/Zombies/ConeheadZombie_Eat.gif"; 
     public static final String ASSETS_Image_FlagZombie_Run = "/Assets/images/Zombies/FlagZombie.gif"; // todo add
     public static final String ASSETS_Image_FlagZombie_Eat = "/Assets/images/Zombies/FlagZombie.gif"; // todo add
     // Assets - Images Cards
--- src/Model/ActCharacter/Zombie/ActConeheadZombie.java
+++ src/Model/ActCharacter/Zombie/ActConeheadZombie.java
@@ -39,9 +39,12 @@ public class ActConeheadZombie implements Act {
     // Attack
     @Override
     public void attack(Object object) {
+        if (zombie.getPath() != zombie.getPath_eat()) { // Fix load lai nhieu anh bi giat
+            zombie.changeImageView(zombie.getPath_eat());
+        }
         Plant plant = (Plant) object;
         plant.setHp(plant.getHp() - zombie.getDame());
-        System.out.println("Plant hp: " + plant.getHp());
+        //System.out.println("Plant hp: " + plant.getHp());
         // Xử lí khi cây bị hết máu
         if (plant.getHp() <= 0) {
             plant.removeImageViewInGridPane();
@@ -51,6 +54,9 @@ public class ActConeheadZombie implements Act {
     // Move
     @Override
     public void move() {
+        if (zombie.getPath() != zombie.getPath_run()) { // Fix load lai nhieu anh bi giat
+            zombie.changeImageView(zombie.getPath_run());
+        }
         zombie.setX(zombie.getX() - zombie.getMove());
         if (zombie.getX() < 10) {
             zombie.removeImageView();
