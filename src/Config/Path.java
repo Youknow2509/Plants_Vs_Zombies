ddd81e1c1b781823c366eee7e546ac41ff29f383
V <lytranvinh.work@gmail.com>
2024-05-16 21:41:25 +0700

    [Upgrade]: Img normal zb eat

--- src/Config/Path.java
+++ src/Config/Path.java
@@ -33,7 +33,7 @@ public class Path {
 
     // Assets - Images Zombies
     public static final String ASSETS_Image_NormalZombie_Run = "/Assets/images/Zombies/NormalZombieRun.gif";
-    public static final String ASSETS_Image_NormalZombie_Eat = "/Assets/images/Zombies/NormalZombieEat.gif"; // todo fix kich thuoc
+    public static final String ASSETS_Image_NormalZombie_Eat = "/Assets/images/Zombies/NormalZombieEat.gif";
     public static final String ASSETS_Image_ConeheadZombie_Run = "/Assets/images/Zombies/NormalZombieRun.gif"; // todo chua co dung tam
     public static final String ASSETS_Image_ConeheadZombie_Eat = "/Assets/images/Zombies/NormalZombieRun.gif"; // todo chua co dung tam
     public static final String ASSETS_Image_FlagZombie_Run = "/Assets/images/Zombies/FlagZombie.gif"; // todo add
