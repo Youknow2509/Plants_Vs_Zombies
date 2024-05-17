91d3f66b405e5c07b7e8f5ea4bd497d67f9f8e53
V <lytranvinh.work@gmail.com>
2024-05-17 16:42:28 +0700

    [Upgrade]: add value creat card

--- src/Help/CardPlants/FactoryCardPlant.java
+++ src/Help/CardPlants/FactoryCardPlant.java
@@ -1,19 +1,23 @@
 package src.Help.CardPlants;
 
 import src.Config.Path;
+import src.Model.Characters.Plants.PeaShooter.PeaShooter;
+import src.Model.Characters.Plants.Repeater.Repeater;
+import src.Model.Characters.Plants.SunFlower.SunFlower;
+import src.Model.Characters.Plants.Wallnut.Wallnut;
 
 public class FactoryCardPlant {
     public static CardPlant createCardPlant(String name) {
         name = name.toUpperCase();
         switch (name) {
             case "SUNFLOWER":
-                return new CardPlant(Path.ASSETS_Image_Card_SunFlower, name);
+                return new CardPlant(Path.ASSETS_Image_Card_SunFlower, name, SunFlower.COST);
             case "PEASHOOTER":
-                return new CardPlant(Path.ASSETS_Image_Card_PeaShooter, name);
+                return new CardPlant(Path.ASSETS_Image_Card_PeaShooter, name, PeaShooter.COST);
             case "WALLNUT":
-                return new CardPlant(Path.ASSETS_Image_Card_WallNut, name);
+                return new CardPlant(Path.ASSETS_Image_Card_WallNut, name, Wallnut.COST);
             case "REPEATER":
-                return new CardPlant(Path.ASSETS_Image_Card_Repeater, name);
+                return new CardPlant(Path.ASSETS_Image_Card_Repeater, name, Repeater.COST);
             default:
                 return null;
         }
