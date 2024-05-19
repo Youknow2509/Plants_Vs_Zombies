c78bb9211402ee36613c16041bb041fbccfce468
V <lytranvinh.work@gmail.com>
2024-05-19 19:06:58 +0700

    [Upgrade]: change default value Conehead in to config and use

--- src/Model/Characters/Zombies/Conehead/ConeheadZombie.java
+++ src/Model/Characters/Zombies/Conehead/ConeheadZombie.java
@@ -1,33 +1,34 @@
 package src.Model.Characters.Zombies.Conehead;
 
+import src.Config.DefaultValue;
 import src.Config.Path;
 import src.Model.ActCharacter.Zombie.ActConeheadZombie;
 import src.Model.Characters.Zombies.Zombie;
 import src.Model.StageCharacter.Zombie.StageCharacterConeheadZombie;
 import src.Utils.LaneToLayoutY;
 
-public class ConeheadZombie extends Zombie { // Todo format lại code
-    // Var information of ConeheadZombie
-    private static final int HP = 400;
-    private static final int DAMAGE = 640;
-    private static final int SPEED_MOVE = 1;
-    private static final int SPEED_ATTACK = 1;
-    private static final int MOVE = 8;
-    private static final int WIDTH = 100;
-    private static final int HEIGHT = 130;
-
+public class ConeheadZombie extends Zombie {
     // Constructor
     public ConeheadZombie() {
         super();
     }
     public ConeheadZombie(int lane) {
-        super(975, 0, Path.ASSETS_Image_ConeheadZombie_Run, WIDTH, HEIGHT, lane, HP, DAMAGE, SPEED_MOVE, SPEED_ATTACK, MOVE, Path.ASSETS_Image_ConeheadZombie_Eat);
+        super(975, 0, Path.ASSETS_Image_ConeheadZombie_Run, DefaultValue.Conehead_WIDTH
+                , DefaultValue.Conehead_HEIGHT, lane, DefaultValue.Conehead_HP
+                , DefaultValue.Conehead_DAMAGE, DefaultValue.Conehead_SPEED_MOVE
+                , DefaultValue.Conehead_SPEED_ATTACK, DefaultValue.Conehead_MOVE
+                , Path.ASSETS_Image_ConeheadZombie_Eat);
         setY(LaneToLayoutY.zombieGetLayoutY(lane));
         setAct(new ActConeheadZombie(this));
         setStageCharacter(new StageCharacterConeheadZombie(this));
     }
     public ConeheadZombie(double x, double y, int lane) {
-        super(x, y, Path.ASSETS_Image_ConeheadZombie_Run, WIDTH, HEIGHT, lane, HP, DAMAGE, SPEED_MOVE, SPEED_ATTACK, MOVE, Path.ASSETS_Image_ConeheadZombie_Eat);
+        super(x, y, Path.ASSETS_Image_ConeheadZombie_Run, DefaultValue.Conehead_WIDTH
+                , DefaultValue.Conehead_HEIGHT, lane, DefaultValue.Conehead_HP
+                , DefaultValue.Conehead_DAMAGE, DefaultValue.Conehead_SPEED_MOVE
+                , DefaultValue.Conehead_SPEED_ATTACK, DefaultValue.Conehead_MOVE
+                , Path.ASSETS_Image_ConeheadZombie_Eat);
+
         setAct(new ActConeheadZombie(this));
         setStageCharacter(new StageCharacterConeheadZombie(this));
     }
