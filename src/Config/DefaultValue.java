451d5cf4213a26d5bada0f2bbf306a053c2a4a05
V <lytranvinh.work@gmail.com>
2024-05-19 19:02:50 +0700

    [Upgrade]: change default value normalZombie in to config and use

--- src/Config/DefaultValue.java
+++ src/Config/DefaultValue.java
@@ -1,2 +1,79 @@
-package src.Config;public class DefaultValue {
+package src.Config;
+
+// Save default values: zb, plant, etc.
+public class DefaultValue {
+    
+// Plants
+
+    // Pea
+    public static int Pea_DAMAGE = 20;
+    public static int Pea_SPEED = 1;
+    public static int Pea_SPEED_ATTACK = 5;
+    public static int Pea_WIDTH = 20;
+    public static int Pea_HEIGHT = 20;
+
+    // PeaShooter
+    public static int PeaShooter_HP = 2000;
+    public static int PeaShooter_DAME = 20;
+    public static int PeaShooter_COST = 100;
+    public static int PeaShooter_SPEEDATTACK = 3;
+    public static int PeaShooter_WIDTH = 60;
+    public static int PeaShooter_HEIGHT = 60;
+    public static int PeaShooter_TIMEBUY = 8;
+
+    // Repeater
+    public static int Repeater_HP = 22000;
+    public static int Repeater_COST = 200;
+    public static int Repeater_DAME = 40;
+    public static int Repeater_SPEEDATTACK = 3;
+    public static int Repeater_WIDTH = 60;
+    public static int Repeater_HEIGHT = 60;
+    public static int Repeater_TIMEBUY = 8;
+
+    // Sun todo
+    public static int Sun_VALUE = 25;
+    public static int Sun_WIDTH = 45;
+    public static int Sun_HEIGHT = 45;
+
+    // Drop Sun todo
+    public static int DropSun_TIMEOUT_DROPSUN = 14000;
+    public static int DropSun_MIN_TIME_DROP = 12;
+    public static int DropSun_MAX_TIME_DROP = 18;
+    public static double DropSun_SPEED = 1;
+
+    // SunFlower
+    public static int SunFlower_HP = 2000;
+    public static int SunFlower_COST = 50;
+    public static int SunFlower_WIDTH = 60;
+    public static int SunFlower_HEIGHT = 60;
+    public static int SunFlower_TIMEOUT_FLOWERSUN = 10000;
+    public static int SunFlower_SPEED_ATTACK = 24000;
+    public static int SunFlower_DAME = 0;
+    public static int SunFlower_TIMEBUY = 8;
+
+    // Wallnut
+    public static int Wallnut_HP = 4000;
+    public static int Wallnut_COST = 50;
+    public static int Wallnut_DAMAGE = 0;
+    public static int Wallnut_WIDTH = 60;
+    public static int Wallnut_HEIGHT = 60;
+    public static int Wallnut_SPEED_ATTACK = 0;
+    public static int Wallnut_TIMEBUY = 15;
+
+// Zombie
+    // Normal zombie
+    public static int NormalZombie_HP = 200;
+    public static int NormalZombie_DAMAGE = 200;
+    public static int NormalZombie_SPEED_MOVE = 2;
+    public static int NormalZombie_SPEED_ATTACK = 1;
+    public static int NormalZombie_MOVE = 8;
+    public static int NormalZombie_WIDTH = 100;
+    public static int NormalZombie_HEIGHT = 130;
+
+    // Conehead zombie
+
+    // Flag zombie
+
+// v.v
+    // LawnMower
 }
--- src/Model/Characters/Zombies/Normal/NormalZombie.java
+++ src/Model/Characters/Zombies/Normal/NormalZombie.java
@@ -1,5 +1,6 @@
 package src.Model.Characters.Zombies.Normal;
 
+import src.Config.DefaultValue;
 import src.Config.Path;
 import src.Model.ActCharacter.Zombie.ActNormalZombie;
 import src.Model.Characters.Zombies.Zombie;
@@ -7,28 +8,28 @@ import src.Model.StageCharacter.Zombie.StageCharacterNormalZombie;
 import src.Utils.LaneToLayoutY;
 
 public class NormalZombie extends Zombie {
-    // Var information of normal zombie
-    private static final int HP = 200;
-    private static final int DAMAGE = 200;
-    private static final int SPEED_MOVE = 2;
-    private static final int SPEED_ATTACK = 1;
-    private static final int MOVE = 8;
-    private static final int WIDTH = 100;
-    private static final int HEIGHT = 130;
 
     // Constructor
     public NormalZombie() {
         super();
     }
     public NormalZombie(int lane) {
-        super(975, 0, Path.ASSETS_Image_NormalZombie_Run, WIDTH, HEIGHT, lane, HP, DAMAGE, SPEED_MOVE, SPEED_ATTACK, MOVE, Path.ASSETS_Image_NormalZombie_Eat);
+        super(975, 0, Path.ASSETS_Image_NormalZombie_Run, DefaultValue.NormalZombie_WIDTH
+                , DefaultValue.NormalZombie_HEIGHT, lane, DefaultValue.NormalZombie_HP
+                , DefaultValue.NormalZombie_DAMAGE, DefaultValue.NormalZombie_SPEED_MOVE
+                , DefaultValue.NormalZombie_SPEED_ATTACK, DefaultValue.NormalZombie_MOVE
+                , Path.ASSETS_Image_NormalZombie_Eat);
         setY(LaneToLayoutY.zombieGetLayoutY(lane));
         setAct(new ActNormalZombie(this));
         setStageCharacter(new StageCharacterNormalZombie(this));
     }
     public NormalZombie(double x, double y, int lane) {
 
-        super(x, y, Path.ASSETS_Image_NormalZombie_Run, WIDTH, HEIGHT, lane, HP, DAMAGE, SPEED_MOVE, SPEED_ATTACK, MOVE, Path.ASSETS_Image_NormalZombie_Eat);
+        super(x, y, Path.ASSETS_Image_NormalZombie_Run, DefaultValue.NormalZombie_WIDTH
+                , DefaultValue.NormalZombie_HEIGHT, lane, DefaultValue.NormalZombie_HP
+                , DefaultValue.NormalZombie_DAMAGE, DefaultValue.NormalZombie_SPEED_MOVE
+                , DefaultValue.NormalZombie_SPEED_ATTACK, DefaultValue.NormalZombie_MOVE
+                , Path.ASSETS_Image_NormalZombie_Eat);
         setAct(new ActNormalZombie(this));
         setStageCharacter(new StageCharacterNormalZombie(this));
     }
