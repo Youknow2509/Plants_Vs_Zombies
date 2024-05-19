0ec162b693284b0c3edf6ddce297098f65adf5cf
V <lytranvinh.work@gmail.com>
2024-05-19 18:33:44 +0700

    [Upgrade]: use default value in config

--- src/Help/CardPlants/FactoryCardPlant.java
+++ src/Help/CardPlants/FactoryCardPlant.java
@@ -1,5 +1,6 @@
 package src.Help.CardPlants;
 
+import src.Config.DefaultValue;
 import src.Config.Path;
 import src.Model.Characters.Plants.PeaShooter.PeaShooter;
 import src.Model.Characters.Plants.Repeater.Repeater;
@@ -11,13 +12,13 @@ public class FactoryCardPlant {
         name = name.toUpperCase();
         switch (name) {
             case "SUNFLOWER":
-                return new CardPlant(Path.ASSETS_Image_Card_SunFlower, name, SunFlower.COST);
+                return new CardPlant(Path.ASSETS_Image_Card_SunFlower, name, DefaultValue.SunFlower_COST);
             case "PEASHOOTER":
-                return new CardPlant(Path.ASSETS_Image_Card_PeaShooter, name, PeaShooter.COST);
+                return new CardPlant(Path.ASSETS_Image_Card_PeaShooter, name, DefaultValue.PeaShooter_COST);
             case "WALLNUT":
-                return new CardPlant(Path.ASSETS_Image_Card_WallNut, name, Wallnut.COST);
+                return new CardPlant(Path.ASSETS_Image_Card_WallNut, name, DefaultValue.Wallnut_COST);
             case "REPEATER":
-                return new CardPlant(Path.ASSETS_Image_Card_Repeater, name, Repeater.COST);
+                return new CardPlant(Path.ASSETS_Image_Card_Repeater, name, DefaultValue.Repeater_COST);
             default:
                 return null;
         }
